5e24b49813b5e5630c580edeae34a5bcde3752a8
jmmaa <reviuy9@gmail.com>
2025-08-23 00:18:25 +0800

    chore: update tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,47 +1,26 @@
 def test_readme_example():
     import dew
 
+    from dew.utils import get_kwargs
+
     result = dew.parse('add rgb color name:"my color" r:100 g:150 b:200')
 
-    assert result["command_name"] == "add"
-    assert result["sub_command_group_name"] == "rgb"
-    assert result["sub_command_name"] == "color"
+    assert result["name"] == "add"
 
-    kwargs = result["kwargs"]
+    # FIX THESE ERROR CASES
+    tail = result["tail"]
 
-    if kwargs is not None:
-        assert kwargs[0][1] == "my color"
-        assert kwargs[1][1] == "100"
-        assert kwargs[2][1] == "150"
-        assert kwargs[3][1] == "200"
+    if isinstance(tail, dict):
+        assert tail["name"] == "rgb"
 
+        tail = tail["tail"]
 
-def test_command_name_only():
-    import dew
+        if isinstance(tail, dict):
+            assert tail["name"] == "color"
 
-    result = dew.parse("add")
+    kwargs = get_kwargs(result)
 
-    assert result["command_name"] == "add"
-    assert result["sub_command_group_name"] is None
-    assert result["sub_command_name"] is None
-
-    assert result["kwargs"] is None
-
-
-def test_command_name_with_kwargs():
-    import dew
-
-    result = dew.parse("add num:3 num:4")
-
-    assert result["command_name"] == "add"
-    assert result["sub_command_group_name"] is None
-    assert result["sub_command_name"] is None
-
-    assert result["kwargs"] is not None
-
-    kwargs = result["kwargs"]
-    if kwargs is not None:
-        assert kwargs[0][0] == "num"
-        assert kwargs[1][0] == "num"
-        assert kwargs[0][1] == "3"
-        assert kwargs[1][1] == "4"
+    assert kwargs[0][1] == "my color"
+    assert kwargs[1][1] == "100"
+    assert kwargs[2][1] == "150"
+    assert kwargs[3][1] == "200"
